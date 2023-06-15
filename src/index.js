da6e540dcff3a40a3785f582f3c7b76c71ab2b05
Barox Phan <75582570+baroxFPTU@users.noreply.github.com>
2023-06-15 23:16:22 +0700

    feat: update optimize image

--- src/index.js
+++ src/index.js
@@ -1,161 +1,81 @@
-import { Image, createCanvas, loadImage, registerFont } from 'canvas';
-import compression from 'compression';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import imagemin from 'imagemin';
-import imageminMozjpeg from 'imagemin-mozjpeg';
-import Jimp from 'jimp';
-import multer, { memoryStorage } from 'multer';
-import { storage } from '../firebase.js';
-
-dotenv.config();
-
-const app = express();
-const upload = multer({ dest: 'upload/', storage: memoryStorage() });
-registerFont('./storage/roboto/roboto-viet-hoa_095802/Roboto-Bold.ttf', {
-  family: 'RobotoBold',
-  weight: 700,
-});
-
-app.use(
-  cors({
-    origin: '*',
-  })
-);
-app.use(compression());
-
-app.post('/api/upload', upload.single('avatar'), async (req, res) => {
-  // Configure
-  const avatarX = 165;
-  const avatarY = 407;
-  const avatarSize = 463;
-  const maxWidthText = 980;
-  const maxHeightText = 920;
-  const textX = 840;
-  const textY = 480;
-  const maxWidthFullName = 442;
-  const maxHeightFullName = 71;
-  const fullNameX = 177;
-  const fullNameY = 910;
-  const maxWidthRole = 527;
-  const maxHeightRole = 56;
-  const roleX = 139;
-  const roleY = 980;
-  const background = await loadImage('./storage/background.png');
-  const canvas = createCanvas(background.width, background.height);
-  const ctx = canvas.getContext('2d');
-  const avatarImage = new Image();
-  const fonts = {
-    sm: '30px "RobotoBold"',
-    md: '40px "RobotoBold"',
-    lg: '50px "RobotoBold"',
-  };
-
-  // Form data
-  const text = req.body.text || 'Thông điệp của bạn...';
-  const fullName = req.body.full_name || 'Tên của bạn...';
-  const role = req.body.role || 'Vai trò của bạn...';
-  const font = text.length <= 300 ? fonts.lg : fonts.md;
-  const fullNameFont =
-    fullName.length <= 14 ? fonts.lg : fullName.length <= 18 ? fonts.md : fonts.sm;
-  const roleFont = fullName.length <= 14 ? fonts.lg : fullName.length <= 18 ? fonts.md : fonts.sm;
-
-  let avatar = null;
-  try {
-    avatar = await Jimp.read(req.file.buffer);
-    avatar.cover(avatarSize, avatarSize);
-    avatar.circle(10000);
-    avatar.crop(0, 0, avatarSize, avatarSize - 36);
-  } catch (error) {
-    console.log(error);
+import { Image as t, createCanvas as e, loadImage as o, registerFont as l } from 'canvas';
+import r from 'compression';
+import a from 'cors';
+import i from 'dotenv';
+import n from 'express';
+import { getDownloadURL as f, ref as p, uploadBytes as m } from 'firebase/storage';
+import s from 'imagemin';
+import g from 'imagemin-mozjpeg';
+import $ from 'jimp';
+import d, { memoryStorage as c } from 'multer';
+import { storage as u } from '../firebase.js';
+i.config();
+let app = n(),
+  upload = d({ dest: 'upload/', storage: c() });
+function drawText(t, e, o, l, r, a) {
+  let i = e.split(' '),
+    n = '',
+    f = 1.6 * t.measureText('M').width,
+    p = [];
+  for (let m = 0; m < i.length; m++) {
+    let s = '' === i[m].trim() ? '' : i[m].trim() + ' ',
+      g = n + s;
+    t.measureText(g).width > r ? (p.push(n), (n = i[m] + ' ')) : (n = g);
   }
-
-  ctx.drawImage(background, 0, 0);
-  if (avatar) {
-    avatarImage.src = await avatar.getBufferAsync(Jimp.MIME_PNG);
-    ctx.drawImage(avatarImage, avatarX, avatarY);
-  }
-
-  ctx.fillStyle = '#121f75';
-  ctx.font = font;
-  drawText(ctx, text, textX, textY, maxWidthText, maxHeightText);
-
-  ctx.fillStyle = 'transparent';
-  ctx.fillRect(fullNameX, fullNameY, maxWidthFullName, maxHeightFullName);
-
-  ctx.font = fullNameFont;
-  ctx.fillStyle = '#ffffff';
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'center';
-  ctx.fillText(fullName, fullNameX + maxWidthFullName / 2, fullNameY);
-
-  ctx.fillStyle = 'transparent';
-  ctx.fillRect(roleX, roleY, maxWidthRole, maxHeightRole);
-
-  ctx.font = roleFont;
-  ctx.fillStyle = '#121f75';
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'center';
-  ctx.fillText(role, roleX + maxWidthRole / 2, roleY);
-
-  const imageRef = ref(storage, 'images/' + req.file.originalname + '.jpg');
-  const compressedImage = await imagemin.buffer(canvas.toBuffer(), {
-    plugins: [
-      imageminMozjpeg({
-        quality: 60,
-        progressive: true, // Enable progressive rendering
-        fastCrush: true, // Use fast DCT methods (less accurate but faster)
-      }),
-    ],
-  });
-  const snapshot = await uploadBytes(imageRef, compressedImage, {
-    contentType: 'image/jpeg',
-  });
-
-  const url = await getDownloadURL(imageRef);
-  // const dataUrl = canvas.toDataURL('image/jpeg');
-  // const out = fs.createWriteStream('./storage/test.jpeg');
-  // const stream = canvas.createJPEGStream();
-  // stream.pipe(out);
-  // out.on('finish', () => console.log('The JPEG file was created.'));
-
-  res.json({
-    message: 'Successfully',
-    url: url,
-  });
-});
-
-function drawText(ctx, text, x, y, maxWidth, maxHeight) {
-  let words = text.split(' '); // split the text into words
-  let line = ''; // current line of text
-  let lineHeight = ctx.measureText('M').width * 1.6; // height of a line based on font size
-  let lines = []; // array of lines
-
-  for (let i = 0; i < words.length; i++) {
-    // loop through each word
-    const word = words[i].trim() === '' ? '' : words[i].trim() + ' ';
-    let testLine = line + word; // add the word to the current line
-    let testWidth = ctx.measureText(testLine).width; // measure the width of the line
-    if (testWidth > maxWidth) {
-      // if it exceeds the maximum width
-      lines.push(line); // push the current line to the array
-      line = words[i] + ' '; // start a new line with the current word
-    } else {
-      line = testLine; // otherwise keep adding words to the current line
-    }
-  }
-  lines.push(line); // push the last line to the array
-
-  for (let i = 0; i < lines.length; i++) {
-    // loop through each line
-    let dy = y + i * lineHeight; // calculate the y position of the line
-    if (dy > maxHeight) break; // if it exceeds the maximum height, stop drawing
-    ctx.fillText(lines[i], x, dy); // draw the line on the canvas
+  p.push(n);
+  for (let $ = 0; $ < p.length; $++) {
+    let d = l + $ * f;
+    if (d > a) break;
+    t.fillText(p[$], o, d);
   }
 }
-
-app.listen(9090, () => console.log('Server running on port 9090'));
-
+l('./storage/roboto/roboto-viet-hoa_095802/Roboto-Bold.ttf', { family: 'RobotoBold', weight: 700 }),
+  app.use(a({ origin: '*' })),
+  app.use(r()),
+  app.post('/api/upload', upload.single('avatar'), async (l, r) => {
+    let a = await o('./storage/background.png'),
+      i = e(a.width, a.height),
+      n = i.getContext('2d'),
+      d = new t(),
+      c = { sm: '30px "RobotoBold"', md: '40px "RobotoBold"', lg: '50px "RobotoBold"' },
+      b = l.body.text || 'Th\xf4ng điệp của bạn...',
+      h = l.body.full_name || 'T\xean của bạn...',
+      x = l.body.role || 'Vai tr\xf2 của bạn...',
+      _ = b.length <= 300 ? c.lg : c.md,
+      w = h.length <= 14 ? c.lg : h.length <= 18 ? c.md : c.sm,
+      y = h.length <= 14 ? c.lg : h.length <= 18 ? c.md : c.sm,
+      T = null;
+    try {
+      (T = await $.read(l.file.buffer)).cover(463, 463), T.circle(1e4), T.crop(0, 0, 463, 427);
+    } catch (B) {
+      console.log(B);
+    }
+    n.drawImage(a, 0, 0),
+      T && ((d.src = await T.getBufferAsync($.MIME_PNG)), n.drawImage(d, 165, 407)),
+      (n.fillStyle = '#121f75'),
+      (n.font = _),
+      drawText(n, b, 840, 480, 980, 920),
+      (n.fillStyle = 'transparent'),
+      n.fillRect(177, 910, 442, 71),
+      (n.font = w),
+      (n.fillStyle = '#ffffff'),
+      (n.textAlign = 'center'),
+      (n.textBaseline = 'center'),
+      n.fillText(h, 398, 910),
+      (n.fillStyle = 'transparent'),
+      n.fillRect(139, 980, 527, 56),
+      (n.font = y),
+      (n.fillStyle = '#121f75'),
+      (n.textAlign = 'center'),
+      (n.textBaseline = 'center'),
+      n.fillText(x, 402.5, 980);
+    let v = p(u, 'images/' + l.file.originalname + '.jpg'),
+      R = await s.buffer(i.toBuffer(), {
+        plugins: [g({ quality: 60, progressive: !0, fastCrush: !0 })],
+      });
+    await m(v, R, { contentType: 'image/jpeg' });
+    let S = await f(v);
+    r.json({ message: 'Successfully', url: S });
+  }),
+  app.listen(9090, () => console.log('Server running on port 9090'));
 export default app;
